6d660c945a1c3c2ee330851e05999c206032fb93
Yahia M. Zakaria <mrzak051@gmail.com>
2026-08-23 22:04:47 +0300

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.ts
@@ -0,0 +1,48 @@
+class MinStack <T>{
+    private stack : T[];
+    private minArr:T[];
+    constructor() {
+        this.stack = []
+        this.minArr = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: T): void {
+        this.stack.push(val)
+        if(this.minArr.length === 0 || this.minArr[this.minArr.length -1] >= val ){
+            this.minArr.push(val)
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.stack.length === 0) return;
+        if(this.minArr[this.minArr.length -1] === this.stack[this.stack.length-1]){
+        this.minArr.pop()
+        this.stack.pop()
+        }else{
+            this.stack.pop()
+
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): T {
+       return this.stack[this.stack.length -1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): T {
+        if(this.minArr.length === 0) return
+        return this.minArr[this.minArr.length -1]
+    }
+}
